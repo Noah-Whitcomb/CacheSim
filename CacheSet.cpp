897431f517e3fed90d6d337dbeab685b3368aff8
noah whitcomb <37048467+Noah-Whitcomb@users.noreply.github.com>
2019-03-24 20:48:45 -0400

    added some stuff

--- CacheSet.cpp
+++ CacheSet.cpp
@@ -37,7 +37,6 @@ bool CacheSet::hit(int tag, int& indexOfHit)
     {
         if (cacheLine[lineNumber].hit(tag)) {
             indexOfHit = lineNumber;
-            cacheLine[lineNumber].setLRU_counter(0);
             return true;
         }
     }
@@ -88,7 +87,7 @@ void CacheSet::loadLine(int inputTag, int offset)
     // figure out which line to write to based on LRU
     int leastRecentlyUsed = cacheLine[0].getLRU_counter();
     int indexToReplace = 0;
-    for(size_t i = 0;i<cacheLine.size();i++)
+    for(size_t i = 1;i<cacheLine.size();i++)
     {
         if(leastRecentlyUsed < cacheLine[i].getLRU_counter())
         {
@@ -96,15 +95,19 @@ void CacheSet::loadLine(int inputTag, int offset)
             indexToReplace = i;
         }
     }
+
+    // this bool here to ensure LRU counter is correct with the line that was just replaced
+    bool index_replaced = false;
     if(cacheLine[indexToReplace].readByte(offset))
     {
         cacheLine[indexToReplace].loadLine(inputTag);
+        index_replaced = true;
     }
 
     // take care of all LRU counters
     for(size_t i = 0;i<cacheLine.size();i++)
     {
-        if(i == indexToReplace)
+        if(i == indexToReplace && index_replaced)
         {
             cacheLine[indexToReplace].setLRU_counter(0);
         }
